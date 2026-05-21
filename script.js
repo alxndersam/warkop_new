c99b00d422d2d01f7e37d3adac6cf753c3a780f2
alxndersam <samsammysam007@gmail.com>
2026-05-22 02:35:48 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,112 @@
-let total = 0;
+// FIREBASE CONFIG
+const firebaseConfig = {
+  apiKey: "AIzaSyDIog8cGM6HWbSj-02rUOh9Wn9JahPix2U",
+  authDomain: "wanmoein-kopi.firebaseapp.com",
+  databaseURL: "https://wanmoein-kopi-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "wanmoein-kopi",
+  storageBucket: "wanmoein-kopi.firebasestorage.app",
+  messagingSenderId: "434124582679",
+  appId: "1:434124582679:web:952776c6a04e6390981152",
+};
 
-function addToCart(name, price){
+firebase.initializeApp(firebaseConfig);
 
-  const cartItems = document.getElementById('cart-items');
+const db = firebase.database();
 
-  const item = document.createElement('div');
+const cart = [];
 
-  item.classList.add('cart-item');
+const cartItems = document.getElementById("cart-items");
+const totalText = document.getElementById("total");
 
-  item.innerHTML = `
-    <span>${name}</span>
-    <span>Rp ${price.toLocaleString('id-ID')}</span>
-  `;
+function addToCart(menu,harga){
 
-  cartItems.appendChild(item);
+  const existing = cart.find(item => item.menu === menu);
 
-  total += price;
-
-  document.getElementById('total').innerText =
-    total.toLocaleString('id-ID');
-}
-
-function payNow(){
-
-  if(total === 0){
-    alert('Keranjang masih kosong 😭');
-    return;
+  if(existing){
+    existing.qty += 1;
+  }else{
+    cart.push({
+      menu,
+      harga,
+      qty:1
+    });
   }
 
-  document.getElementById('success').style.display = 'block';
+  renderCart();
 }
+
+function renderCart(){
+
+  cartItems.innerHTML = "";
+
+  let total = 0;
+
+  cart.forEach(item => {
+
+    total += item.harga * item.qty;
+
+    cartItems.innerHTML += `
+      <div class="cart-item">
+
+        <div>
+          ${item.menu}
+          (${item.qty}x)
+        </div>
+
+        <div>
+          Rp ${(item.harga * item.qty).toLocaleString('id-ID')}
+        </div>
+
+      </div>
+    `;
+  });
+
+  totalText.innerText = total.toLocaleString('id-ID');
+}
+
+document
+  .getElementById("orderForm")
+  .addEventListener("submit", async (e) => {
+
+    e.preventDefault();
+
+    if(cart.length === 0){
+      alert("Keranjang masih kosong 😭");
+      return;
+    }
+
+    const nama = document.getElementById("nama").value;
+    const meja = document.getElementById("meja").value;
+
+    const snapshot = await db.ref("orders").once("value");
+
+    const antrian = snapshot.numChildren() + 1;
+
+    let total = 0;
+
+    cart.forEach(item => {
+      total += item.harga * item.qty;
+    });
+
+    const data = {
+      nama,
+      meja,
+      antrian,
+      status:"pending",
+      orders:cart,
+      total,
+      createdAt:Date.now()
+    };
+
+    await db.ref("orders").push(data);
+
+    alert(
+      `Pesanan berhasil ☕\n\nNomor Antrian: ${antrian}`
+    );
+
+    cart.length = 0;
+
+    renderCart();
+
+    document.getElementById("orderForm").reset();
+});
